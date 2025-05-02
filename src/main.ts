8ef9527b32d56ca5d80999f4fd4c347e1ad601d6
2382038 <2382038@unai.edu>
2025-05-02 14:21:17 +0700

    enabled cors for all url

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'http://localhost:5173',
-  });
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Simple Social Media API')
